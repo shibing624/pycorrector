b49669b22c48c34d41964c1e402c95dc0bc08f0b
xuming06 <507153809@qq.com>
2018-03-06 22:26:39 +0800

    update setup version.

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.md'), encoding='utf-8') as f:
 
 setup(
     name='pycorrector',
-    version='0.0.3',
+    version='0.0.4',
     description='Chinese Text Error corrector',
     long_description=long_description,
     author='XuMing',
