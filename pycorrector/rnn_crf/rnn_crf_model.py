ee47125e34737db84aafe4908ebf60f052e4f023
xuming06 <507153809@qq.com>
2018-04-12 09:27:40 +0800

    update save model each epoch.

--- pycorrector/rnn_crf/rnn_crf_model.py
+++ pycorrector/rnn_crf/rnn_crf_model.py
@@ -2,7 +2,11 @@
 # Author: XuMing <xuming624@qq.com>
 # Brief: BiLSTM-CRF
 
-from keras.layers import Embedding, Bidirectional, LSTM
+from keras.callbacks import LambdaCallback
+from keras.callbacks import ModelCheckpoint
+from keras.layers import Bidirectional
+from keras.layers import Embedding
+from keras.layers import LSTM
 from keras.models import Sequential
 from keras_contrib.layers import CRF
 from keras_contrib.utils import save_load_utils
@@ -31,5 +35,21 @@ def save_model(model, save_model_path):
 def load_model(word_dict, label_dict, embedding_dim, rnn_hidden_dim, dropout, save_model_path):
     # https://github.com/keras-team/keras-contrib/issues/125
     model = create_model(word_dict, label_dict, embedding_dim, rnn_hidden_dim, dropout)
-    save_load_utils.load_all_weights(model, save_model_path, include_optimizer=False)
+    # save_load_utils.load_all_weights(model, save_model_path, include_optimizer=False) # one way
+    model.load_weights(save_model_path)  # another way
     return model
+
+
+def callback(save_model_path, logger=None):
+    # Print the batch number at the beginning of every batch.
+    if logger:
+        batch_print_callback = LambdaCallback(
+            on_batch_begin=lambda batch, logs: logger.info('batch: %d' % batch))
+    else:
+        batch_print_callback = LambdaCallback(
+            on_batch_begin=lambda batch, logs: print(batch))
+    # define the checkpoint, save model
+    checkpoint = ModelCheckpoint(save_model_path, monitor='val_acc', verbose=1,
+                                 save_best_only=True, mode='auto',
+                                 save_weights_only=True, period=1)
+    return [batch_print_callback, checkpoint]
--- pycorrector/rnn_crf/train.py
+++ pycorrector/rnn_crf/train.py
@@ -2,15 +2,14 @@
 # Author: XuMing <xuming624@qq.com>
 # Brief:
 import numpy as np
-from sklearn.model_selection import train_test_split
 
 import rnn_crf_config as config
 from data_reader import build_dict
 from data_reader import load_dict
 from data_reader import pad_sequence
 from data_reader import vectorize_data
+from rnn_crf_model import callback
 from rnn_crf_model import create_model
-from rnn_crf_model import save_model
 from utils.io_utils import get_logger
 
 logger = get_logger(__name__)
@@ -48,25 +47,21 @@ def train(train_word_path=None,
     label_ids = vectorize_data(train_label_path, label_ids_dict)
     # pad sequence
     word_seq, label_seq = pad_sequence(word_ids, label_ids, maxlen=maxlen)
-    # split train test
-    X_train, X_test, y_train, y_test = train_test_split(
-        word_seq, label_seq, test_size=0.2, random_state=42)
-    # reshape for crf model use
-    y_train = np.reshape(y_train, (y_train.shape[0], y_train.shape[1], 1))
-    y_test = np.reshape(y_test, (y_test.shape[0], y_test.shape[1], 1))
+    # reshape label for crf model use
+    label_seq = np.reshape(label_seq, (label_seq.shape[0], label_seq.shape[1], 1))
     logger.info("Data loaded.")
-
+    # model
     logger.info("Training BILSTM_CRF model...")
     model = create_model(word_ids_dict, label_ids_dict,
                          embedding_dim, rnn_hidden_dim, dropout)
+    # callback
+    callbacks_list = callback(save_model_path, logger)
     # fit
-    model.fit(X_train, y_train,
+    model.fit(word_seq, label_seq,
               batch_size=batch_size,
               epochs=epoch,
-              validation_data=[X_test, y_test])
-    # save model
-    save_model(model, save_model_path)
-    logger.info("model saved: %s" % save_model_path)
+              validation_split=0.2,
+              callbacks=callbacks_list)
     logger.info("Training has finished.")
 
 
