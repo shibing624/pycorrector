4474f6b88e3663749367a283e7f481644389f02a
xuming06 <507153809@qq.com>
2018-05-22 01:09:33 +0800

    update lm infer with ppl.

--- pycorrector/rnn_lm/infer.py
+++ pycorrector/rnn_lm/infer.py
@@ -50,7 +50,7 @@ def generate(begin_word):
             word = to_word(predict, vocabularies)
         sentence = ''
         i = 0
-        while word != END_TOKEN and word != START_TOKEN and word!=UNK_TOKEN:
+        while word != END_TOKEN and word != START_TOKEN and word != UNK_TOKEN:
             sentence += word
             i += 1
             if i >= 24:
@@ -68,19 +68,15 @@ def generate(begin_word):
         return sentence
 
 
-def ppl(text):
-    perplexity = 0
-    batch_size = 1
+def ppl(sentence_list):
+    ppl_list = []
+    # load data dict
     word_to_int = load_word_dict(conf.word_dict_path)
-    # data idx
-    x = [word_to_int[c] if c in word_to_int else word_to_int[UNK_TOKEN] for c in text]
-    x = [word_to_int[START_TOKEN]] + x + [word_to_int[END_TOKEN]]
-    # reshape
-    y = np.array(x[1:]).reshape((-1, batch_size))
-    x = np.array(x[:-1]).reshape((-1, batch_size))
-
+    # init params
+    batch_size = 1
     input_data = tf.placeholder(tf.int32, [batch_size, None])
     output_targets = tf.placeholder(tf.int32, [batch_size, None])
+    # init model
     end_points = rnn_model(model='lstm',
                            input_data=input_data,
                            output_data=output_targets,
@@ -93,22 +89,46 @@ def ppl(text):
     saver = tf.train.Saver(tf.global_variables())
     init_op = tf.group(tf.global_variables_initializer(), tf.local_variables_initializer())
     with tf.Session() as sess:
+        # init op
         sess.run(init_op)
         checkpoint = tf.train.latest_checkpoint(conf.model_dir)
         saver.restore(sess, checkpoint)
         print("loading model from the checkpoint {0}".format(checkpoint))
-        for i in range(x.shape[0]):
-            [perplexity] = sess.run([end_points['perplexity']],
-                                    feed_dict={input_data: x[i:i + 1, :],
-                                               output_targets: y[i:i + 1, :]})
-        print('perplexity: {0}'.format(perplexity))
-    return perplexity
+
+        # infer each sentence
+        for sentence in sentence_list:
+            ppl = 0
+            # data idx
+            x = [word_to_int[c] if c in word_to_int else word_to_int[UNK_TOKEN] for c in sentence]
+            x = [word_to_int[START_TOKEN]] + x + [word_to_int[END_TOKEN]]
+            print('x:', x)
+            # reshape
+            y = np.array(x[1:]).reshape((-1, batch_size))
+            x = np.array(x[:-1]).reshape((-1, batch_size))
+            print(x.shape)
+            print(y.shape)
+            # get each word perplexity
+            for i in range(x.shape[0]):
+                perplexity = sess.run(end_points['perplexity'],
+                                      feed_dict={input_data: x[i:i + 1, :],
+                                                 output_targets: y[i:i + 1, :]})
+                print('{0} -> {1}, perplexity: {2}'.format(x[i:i + 1, :], y[i:i + 1, :], perplexity))
+                ppl += perplexity
+            ppl = ppl / x.shape[0]
+            print('perplexity:' + str(ppl))
+            ppl_list.append(ppl)
+    return ppl_list
 
 
-if __name__ == '__main__':
+def infer_generate():
     # begin_char = input('please input the first character:')
     begin_char = '我'
     print(generate(begin_char))
-    # ppl("化肥和农药不仅对人类有害，而且对地球的土壤也非常有害。")
-    # ppl("化肥和农药不仅对土壤也非常。")
-    # ppl("化肥和农药、不仅对人、类这最化有害。")
+
+
+if __name__ == '__main__':
+    sentences = ['化肥和农药不仅对人类有害',  # perplexity:4277.740985026727
+                 '化肥和浓药不仅对人类有害',  # perplexity:5492552.78279785
+                 '化肥和浓药不仅对人类有海',  # perplexity:5505405.811695649
+                 '化肥和农药不仅对人类有害，而且对海洋危害很大']  # perplexity:21851.84118722833
+    ppl(sentences)
