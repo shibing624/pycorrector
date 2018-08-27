5cb3c1189a3631657249570d6f6129177440a7cc
Kun Qian <kun@Kuns-MacBook-Pro.local>
2018-08-27 14:20:18 +0800

    lm path change

--- pycorrector/config.py
+++ pycorrector/config.py
@@ -1,12 +1,10 @@
 # -*- coding: utf-8 -*-
-# Author: XuMing <xuming624@qq.com>
-# Brief: configuration
 import os
 
 # word dictionary file
-# word_dict_path = 'data/word_dict.txt'
-word_dict_path = 'data/360wan-utf8.dict'
-#word_dict_path = '../jieba/extra_dict/dict.txt.big'
+word_dict_path = 'data/word_dict.txt'
+# word_dict_path = 'data/360wan-utf8.dict'
+
 # word dictionary model file path, file will be built from word_freq_path
 word_dict_model_path = 'data/word_dict.pkl'
 # char set file
@@ -22,13 +20,9 @@ same_stroke_model_path = 'data/same_stroke.pkl'
 
 # language model path
 
+language_model_path = 'data/kenlm/nlpcc_char.klm'
 
-# language_model_path = 'data/kenlm/nlpcc_char.klm'
-
-language_model_path = 'data/kenlm/nlpcc_char_5gram.klm'
+# language_model_path = 'data/kenlm/nlpcc_char_5gram.klm'
 # language_model_path = 'data/kenlm/nlpcc_char_7gram.klm'
 
 
-# reverse lm
-# word_dict_path = 'data/rev_360wan-utf8.dict'
-# language_model_path = 'data/kenlm/rev_nlpcc_char.klm'
