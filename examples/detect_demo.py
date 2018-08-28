1993ea7cd6f35d47541653dc35b5eef88810d630
xuming06 <507153809@qq.com>
2018-08-28 17:37:31 +0800

    add detect demo.

---
+++ examples/detect_demo.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+@author:XuMing（xuming624@qq.com)
+@description: 
+"""
+
+import pycorrector
+
+idx_errors = pycorrector.detect('少先队员因该为老人让坐')
+print(idx_errors)
--- pycorrector/__init__.py
+++ pycorrector/__init__.py
@@ -3,5 +3,6 @@
 # Brief: pycorrector.api
 
 from .corrector import correct
+from .detector import detect
 
 __version__ = '0.1.3'
