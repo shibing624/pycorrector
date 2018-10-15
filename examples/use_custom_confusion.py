11dd1c5633b8f1e084db23c64b033768b157a5a6
xuming06 <507153809@qq.com>
2018-10-15 16:41:39 +0800

    update demo.

--- examples/use_custom_confusion.py
+++ examples/use_custom_confusion.py
@@ -16,7 +16,7 @@
 import pycorrector
 
 error_sentences = [
-    '买iPhone差多少钱',
+    '买iPhone差，要多少钱',
     '我想喝小明同学。',
     '哪里卖苹果吧？请大叔给我让坐',
     '我对于宠物出租得事非常认同',
@@ -39,14 +39,15 @@ for line in error_sentences:
     correct_sent = pycorrector.correct(line)
     print("original sentence:{} => correct sentence:{}".format(line, correct_sent))
 
-# original sentence:买iPhone差多少钱 => correct sentence:('买iPhone此多少钱', [['差', '此', 7, 8]])
+
+# original sentence:买iPhone差，要多少钱 => correct sentence:('买iPhone此，要多少钱', [['差', '此', 7, 8]])
 # original sentence:我想喝小明同学。 => correct sentence:('我像喝晓明同学。', [['我想', '我像', 0, 2], ['喝小明', '喝晓明', 2, 5]])
 # original sentence:哪里卖苹果吧？请大叔给我让坐 => correct sentence:('哪里卖苹果吧？请大叔给我让座', [['坐', '座', 13, 14]])
 # original sentence:我对于宠物出租得事非常认同 => correct sentence:('我对于宠物出租得是非常认同', [['得事', '得是', 7, 9]])
 # original sentence:天地无垠大，我们的舞台无线大 => correct sentence:('天地无限贷，我们的舞台无限大', [['天地无垠', '天地无限', 0, 4], ['大', '贷', 4, 5], ['无线大', '无限大', 11, 14]])
 # original sentence:交通先行了怎么过去啊？ => correct sentence:('交通先行了怎么过去啊？', [])
 # *****************************************************
-# original sentence:买iPhone差多少钱 => correct sentence:('买iphoneX多少钱', [['iphone差', 'iphoneX', 1, 8]])
+# original sentence:买iPhone差，要多少钱 => correct sentence:('买iphoneX，要多少钱', [['iphone差', 'iphoneX', 1, 8]])
 # original sentence:我想喝小明同学。 => correct sentence:('我想喝小茗同学。', [['小明同学', '小茗同学', 3, 7]])
 # original sentence:哪里卖苹果吧？请大叔给我让坐 => correct sentence:('哪里卖苹果八？请大叔给我让座', [['苹果吧', '苹果八', 3, 6], ['坐', '座', 13, 14]])
 # original sentence:我对于宠物出租得事非常认同 => correct sentence:('我对于宠物出租的事非常认同', [['得事', '的事', 7, 9]])
