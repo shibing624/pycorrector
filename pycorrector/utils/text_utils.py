79f89d650690d4a1bc7785809be12521b7f8185a
CornWu <CornWu@yeah.net>
2019-08-06 10:52:09 +0800

    Update text_utils.py
    转换编码后再做判断

--- pycorrector/utils/text_utils.py
+++ pycorrector/utils/text_utils.py
@@ -10,8 +10,10 @@ from pycorrector.utils.langconv import Converter
 
 
 def is_chinese(uchar):
-    """判断一个unicode是否是汉字"""
-    if '\u4e00' <= uchar <= '\u9fa5':
+    if uchar in '\r\n#$%&*+,-./\:;|，。！？——：；％￥＃＆、｜1234567890':
+        return True
+    cd = uchar.decode('utf-8')
+    if cd >= u'\u4e00' and cd <= u'\u9fa5':
         return True
     else:
         return False
