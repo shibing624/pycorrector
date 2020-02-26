084cfe29f64902081f5895c00bcd956a23298d86
xuming624 <xuming624@qq.com>
2020-02-26 20:12:34 +0800

    update demo.

--- examples/load_custom_language_model.py
+++ examples/load_custom_language_model.py
@@ -10,16 +10,18 @@ sys.path.append("../")
 from pycorrector import Corrector
 
 pwd_path = os.path.abspath(os.path.dirname(__file__))
-model = Corrector(language_model_path=pwd_path + '/../pycorrector/data/people_chars_lm.klm')
+lm_path = os.path.join(pwd_path, '../pycorrector/data/people_chars_lm.klm')
+model = Corrector(language_model_path=lm_path)
 
-error_sentences = [
-    '少先队员因该为老人让坐',
-    '天地无垠大，我们的舞台无线大',
-    '我的形像代言人',
-    '我的形像坏人吗',
-    '这么做为了谁？',
-    '做为一个男人'
-]
-for line in error_sentences:
-    correct_sent = model.correct(line)
-    print("original sentence:{} => correct sentence:{}".format(line, correct_sent))
+if __name__ == '__main__':
+    error_sentences = [
+        '少先队员因该为老人让坐',
+        '天地无垠大，我们的舞台无线大',
+        '我的形像代言人',
+        '我的形像坏人吗',
+        '这么做为了谁？',
+        '做为一个男人'
+    ]
+    for line in error_sentences:
+        correct_sent = model.correct(line)
+        print("original sentence:{} => correct sentence:{}".format(line, correct_sent))
