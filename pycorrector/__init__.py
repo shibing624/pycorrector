e8535814bd246a5b35d3a2eb22728c9255b344a4
xuming624 <xuming624@qq.com>
2021-04-27 11:20:08 +0800

    del torch import.

--- pycorrector/__init__.py
+++ pycorrector/__init__.py
@@ -2,15 +2,9 @@
 # Author: XuMing(xuming624@qq.com)
 # Brief: init
 
-from .bert import bert_corrector
-from .bert.bert_corrector import BertCorrector
 from .config import language_model_path
 from .corrector import Corrector
 from .en_spell import en_correct
-# from .ernie import ernie_corrector
-# from .ernie.ernie_corrector import ErnieCorrector
-from .macbert import macbert_corrector
-from .macbert.macbert_corrector import MacBertCorrector
 from .utils import text_utils, get_file, tokenizer, io_utils, math_utils
 from .utils.logger import set_log_level
 from .utils.text_utils import get_homophones_by_char, get_homophones_by_pinyin, traditional2simplified, \
