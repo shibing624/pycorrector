70a2024052ced6e15d9372848da3c9a87d19e914
shibing624 <shibing624@126.com>
2022-03-22 17:12:18 +0800

    add proper correct case.

--- examples/base_demo.py
+++ examples/base_demo.py
@@ -19,6 +19,9 @@ if __name__ == '__main__':
         '机七学习是人工智能领遇最能体现智能的一个分知',
         '一只小鱼船浮在平净的河面上',
         '我的家乡是有明的渔米之乡',
+        '这场比赛我甘败下风',
+        '这家伙还蛮格尽职守的',
+        '报应接中迩来',  # 接踵而来
     ]
     for line in error_sentences:
         correct_sent, err = pycorrector.correct(line)
